c32b78829f630498422fbcd819cc107d59ad9065
MrMouse2405 <68935395+MrMouse2405@users.noreply.github.com>
2023-12-31 19:45:08 -0500

    Added new queries

--- paulbadman/bot.py
+++ paulbadman/bot.py
@@ -14,7 +14,8 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=botconstants.COMMAND_PREFIX, description=botconstants.BOT_DESCRIPTION, intents=intents)
+bot = commands.Bot(command_prefix=botconstants.COMMAND_PREFIX,
+                   description=botconstants.BOT_DESCRIPTION, intents=intents)
 
 """
 
@@ -22,9 +23,11 @@ bot = commands.Bot(command_prefix=botconstants.COMMAND_PREFIX, description=botco
 
     Category: Funny
 """
+
+
 class Funny(commands.Cog):
     """
-    
+
         Funny
 
     """
@@ -33,8 +36,9 @@ class Funny(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def rights(self,ctx):
+    async def rights(self, ctx):
         await ctx.send(botconstants.RIGHTS)
+
 
 """
 
@@ -43,9 +47,11 @@ class Funny(commands.Cog):
     Category: Attachments
 
 """
+
+
 class Attachments(commands.Cog):
     """
-    
+
         Commands for Saving Attachments
 
     """
@@ -54,28 +60,29 @@ class Attachments(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def howsave(self,ctx : commands.Context):
+    async def howsave(self, ctx: commands.Context):
         await ctx.send(f"{ctx.author.mention} this how https://media.discordapp.net/attachments/910216279212834907/1186815470691700877/image.png?ex=65949f0a&is=65822a0a&hm=79ee2a37b8a4f90a65c3d1588a418143e44001600f2af0c8aad7059aaf6c15f8&=&format=webp&quality=lossless&width=607&height=687")
 
+    @commands.command()
+    async def save(self, ctx: commands.Context, name: str):
+        await ctx.send(save_attachment.add_attachment(ctx, name))
 
     @commands.command()
-    async def save(self,ctx : commands.Context, name : str):
-        await ctx.send(save_attachment.add_attachment(ctx,name))
-
-
-    @commands.command()
-    async def get(self,ctx : commands.Context, name : str):
-        await ctx.send(await save_attachment.get_attachment(ctx,name))
+    async def get(self, ctx: commands.Context, name: str):
+        await ctx.send(await save_attachment.get_attachment(ctx, name))
 
     @commands.command()
-    async def la(self,ctx : commands.Context):
+    async def la(self, ctx: commands.Context):
         await ctx.send(save_attachment.list_saved(ctx))
+
 
 """
 
     Bot Events
 
 """
+
+
 @bot.event
 async def on_ready():
     await bot.add_cog(Funny(bot))
@@ -83,15 +90,14 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
     result = check_for_triggers(message.content)
     if result:
-        await message.channel.send(result)  
-
-    
+        await message.channel.send(result)
 
 
 """
@@ -100,4 +106,4 @@ async def on_message(message):
 
 """
 
-bot.run('')
+bot.run('bot-token')
--- paulbadman/programmed_responses.py
+++ paulbadman/programmed_responses.py
@@ -1,65 +1,34 @@
 from fuzzywuzzy import process
-from botconstants import RIGHTS, BOT_DESCRIPTION
+import os
+import tomllib
 
-        
-Queries = [
-    (
-        ["lawyer"],
-        "Better Call Saul!"
-    ),
-    (
-        ["oh Jimmy"],
-        "Don't you fucking Oh, Jimmy me!"
-    ),
-    (
-        ["how?"],
-        "First thing's first, you're gonna put a dollar in my pocket. Both of you."
-    ),
-    (
-        ["skylar"],
-        "Skyler! It's a beautiful name! Reminds me of a... big beautiful sky!"
-    ),
-    (
-        ["good to meet you","nice to meet you","hello"],
-        "Don't drink and drive, but if you do, call me!"
-    ),
-    (
-        ["Who can I sue"],
-        "Who can you sue? Try police departments, libraries, construction companies, school officials, cleaning services, financial institutions (local and international), your neighbors, your family members, your church, synagogue or other religious institution, your employers, your employers' customers, suppliers, companies in other countries; companies that made the drugs that were turned into the drugs that you took. The possibilities are limitless!"
-    ),
-    (
-        ["rights"],
-       RIGHTS
-    ),
-    (
-        ["paul badman"],
-       BOT_DESCRIPTION
-    )
-]
 
+KEEP_YOUR_VOICE_LOW = "keep your voice low"
 
 TRIGGERS = []
 RESPONSES = {}
 
-for query in Queries:
-    response = query[1]
-    for trigger in query[0]:
-        TRIGGERS.append(trigger)
-        RESPONSES[trigger] = response
+with open(os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "Queries.toml", 'rb') as fp:
+    Queries = tomllib.load(fp)
+    for query in Queries:
+        for trigger in Queries.get(query).get("triggers"):
+            TRIGGERS.append(trigger)
+            RESPONSES[trigger] = query
+    del Queries
 
 
-del Queries
-
-
-def check_for_triggers(str):
+def check_for_triggers(string: str):
     """
         Returns message if triggerd,
             or else returns none
 
     """
-    result = process.extractOne(str, TRIGGERS, score_cutoff = 80)
-    
+    if string.isupper():
+        return "Keep your voice low"
+
+    result = process.extractOne(string, TRIGGERS, score_cutoff=90)
+
     if result:
         return RESPONSES[result[0]]
-    
+
     return None
